910b9086a9a3cc00a640e0cd83c5676010d27c06
Thomas Schäfer <git@tschaefer.eu>
2020-11-23 19:20:00 +0100

    Update upgrade icon style to match timeline

--- src/Components/Common/Icons/UpgradeIcon.tsx
+++ src/Components/Common/Icons/UpgradeIcon.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import img from '../../../assets/images/upgrades/Protoss/Air_armor_1.gif'
+import styles from './upgrade-icon.module.scss'
+import classNames from 'classnames'
 
 interface Props {
   id: string
@@ -9,7 +11,7 @@ interface Props {
 
 const UpgradeIcon: React.FC<Props> = ({ id, amount, image }) => {
   return (
-    <div className='m-2'>
+    <div className={classNames('m-2', styles.wrapper)}>
       <div className='d-flex justify-content-center'>
         <img src={image} style={{ width: 48, height: 48 }} alt={id} />
       </div>
