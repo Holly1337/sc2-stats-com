eb166cbfecdef168edf67e569511f9a427b4c473
thomas.schaefer <thomas.schaefer@abl-solutions.com>
2020-12-04 16:35:50 +0100

    Update styling of UnitCount.tsx and add number formatting

--- src/Components/Sections/Units/UnitsCount/UnitCount.tsx
+++ src/Components/Sections/Units/UnitsCount/UnitCount.tsx
@@ -3,6 +3,7 @@ import { Header } from 'semantic-ui-react'
 import { unitIcons } from '../../../Common/Icons/Buildings/unitIcons'
 import { buildingIcons } from '../../../Common/Icons/Buildings/buildingIcons'
 import Image from 'next/image'
+import FormattedNumber from '../../../Common/Numbers/FormattedNumber'
 
 interface Props {
   id: string
@@ -27,12 +28,12 @@ export const UnitCount = (props: Props) => {
   // TODO: hover for resources
 
   return (
-      <div className={'d-flex flex-column align-items-center'} style={{ width: 136, margin: 20, padding: 10, borderRadius: 10, border: '1px solid darkgray', backgroundColor: 'lightgray' }}>
+      <div className={'d-flex flex-column align-items-center'} style={{ width: 136, margin: 20, padding: 10, borderRadius: 10, border: '1px solid darkgray', backgroundColor: '#f3f3f3' }}>
         {/*<Header size={'medium'} textAlign={'center'} inverted={true} color={'grey'}>{props.name}</Header>*/}
-        <div style={{ borderRadius: 5, padding: 10, backgroundColor: 'black' }}>
+        <div style={{ borderRadius: 10, padding: 10, backgroundColor: 'black' }}>
           <Image src={`/assets/images/icons/unitsAndBuildings/${imgSrc}`} width={76} height={76} alt={props.name}/>
         </div>
-        <Header size={'medium'} textAlign={'center'}>{props.count}</Header>
+        <Header size={'medium'} textAlign={'center'} style={{ marginTop: 10 }}><FormattedNumber value={props.count}/></Header>
       </div>
   )
 }
