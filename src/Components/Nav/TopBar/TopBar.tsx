03a88b1e53a5d69b5b727a462f420e4abb349209
Thomas Schäfer <git@tschaefer.eu>
2020-12-06 12:27:45 +0100

    Add Twitter icon to TopBar

--- src/Components/Nav/TopBar/TopBar.tsx
+++ src/Components/Nav/TopBar/TopBar.tsx
@@ -1,19 +1,27 @@
 import styles from '../topbar.module.scss'
 import Link from 'next/link'
 import { SearchBar } from './SearchBar'
+import { Header, Icon } from 'semantic-ui-react'
 
 export const TopBar = () => {
-    return (
-        <div className={styles.topBar}>
-            <Link href={'/'}>
-              <a>
-                <span style={{fontSize: 18}}>Home</span>
-              </a>
-            </Link>
-          <div>
-            <SearchBar />
-          </div>
-          <div>Twitter here</div>
-        </div>
-    )
+  return (
+    <div className={styles.topBar}>
+      <div>
+        <Link href={'/'}>
+          <a>
+            <Icon name={'line graph'} size={'large'} className={'mr-4'} />
+            <Header size={'small'} style={{ display: 'inline-block', marginTop: 1 }}>SC2 Stats</Header>
+          </a>
+        </Link>
+      </div>
+      <div>
+        <SearchBar />
+      </div>
+      <div>
+        <a href="https://twitter.com/HollySC2">
+          <Icon name={'twitter'} circular size={'large'} />
+        </a>
+      </div>
+    </div>
+  )
 }
