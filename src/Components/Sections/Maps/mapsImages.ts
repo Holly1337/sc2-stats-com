1d4f064319862ec44e9b460e112571be2a0f91a2
Thomas Schäfer <git@tschaefer.eu>
2020-12-07 12:06:21 +0100

    Add more maps to mapsImages.ts

--- src/Components/Sections/Maps/mapsImages.ts
+++ src/Components/Sections/Maps/mapsImages.ts
@@ -21,5 +21,38 @@ export const mapImagePaths: { [id: string]: string } = {
   deathaura,
   iceAndChrome,
   pillarsOfGold,
-  submarine
+  submarine,
+  'Eternal Empire LE': eternalEmpire,
+  'Simulacrum LE': simulacrum,
+  'Night Shade LE': nightshade,
+  'Ever Dream LE': everDream,
+  'Golden Wall LE': goldenWall,
+  'Zen LE': zen,
+  'Purity and Industry LE': purityAndIndustry,
+  'Deathaura LE': deathaura,
+  'Ice and Chrome LE': iceAndChrome,
+  'Pillars of Gold LE': pillarsOfGold,
+  'Submarine LE': submarine
+}
+
+const fallbackImage = '/assets/images/100x100.png'
+export const getMapImageByName = (mapName: string) => {
+  let path = mapImagePaths[mapName]
+  if (typeof path === 'string') {
+    return path
+  }
+  // try to find map name with LE
+  let mapNameLE = `${mapName} LE`
+  path = mapImagePaths[mapNameLE]
+  if (typeof path === 'string') {
+    return path
+  }
+  // try to remove spaces and turn first latter lowercase
+  let mapNameFallback = mapName.replace(' ', '')
+  mapNameFallback = mapName.charAt(0).toLocaleLowerCase() + mapName.slice(1)
+  path = mapImagePaths[mapNameFallback]
+  if (typeof path === 'string') {
+    return path
+  }
+  return fallbackImage
 }
