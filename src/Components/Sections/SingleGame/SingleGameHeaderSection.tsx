acd57cde5ca06dd741262916a419a24bd4717b04
Thomas Schäfer <git@tschaefer.eu>
2020-12-11 22:38:11 +0100

    Change player name border to player color. Add icons to show winner/loser

--- src/Components/Sections/SingleGame/SingleGameHeaderSection.tsx
+++ src/Components/Sections/SingleGame/SingleGameHeaderSection.tsx
@@ -2,49 +2,63 @@ import React from 'react'
 import { raceMap } from '../../../util/raceMap'
 import styles from './game.module.scss'
 import { PlayerName } from './PlayerName'
-import classNames from 'classnames'
+import { Icon } from 'semantic-ui-react'
+import { mrgbaToRgbaString } from '../../../util/colors'
 
 interface Props {
   matchMeta: MatchMeta
 }
 
+const WinnerIcon = () => <Icon name={'angle double up'} color={'green'} size={'large'} className={styles.resultIcon} />
+const LoserIcon = () => <Icon name={'angle double down'} color={'red'} size={'large'} className={styles.resultIcon} />
+
 export const SingleGameHeaderSection = (props: Props) => {
   const { matchMeta } = props
-  const { players, racesShort } = matchMeta
-
-  const classesP1 = classNames(
-    styles.p1,
-    matchMeta.results[0] === 'Win' ? styles.winner : styles.loser
-  )
-
-  const classesP2 = classNames(
-    styles.p2,
-    matchMeta.results[1] === 'Win' ? styles.winner : styles.loser
-  )
-
-  const classesVs = classNames(
-    styles.vs,
-    matchMeta.results[0] === 'Win' ? styles.vsWinLose : styles.vsLoseWin
-  )
+  const { players, racesShort, colors } = matchMeta
 
   return (
     <>
       <div className={styles.headerWrapper}>
-        <div className={classesP1}>
+        <div
+          className={styles.p1}
+          style={{
+          borderColor: mrgbaToRgbaString(colors[0])
+          }}
+        >
           <div>
-            <PlayerName>{players[0]}</PlayerName>
+            {matchMeta.results[0] === 'Win' ? <WinnerIcon /> : <LoserIcon />}
           </div>
-          <div className={styles.race}>
-            {raceMap[racesShort[0]]}
+          <div>
+            <div>
+              <PlayerName>{players[0]}</PlayerName>
+            </div>
+            <div className={styles.race}>
+              {raceMap[racesShort[0]]}
+            </div>
           </div>
         </div>
-        <div className={classesVs}>vs</div>
-        <div className={classesP2}>
+        <div
+          className={styles.vs}
+          style={{
+            borderImageSource: `linear-gradient(to right, ${mrgbaToRgbaString(colors[0])}, ${mrgbaToRgbaString(colors[1])})`
+          }}
+        >vs</div>
+        <div
+          className={styles.p2}
+          style={{
+            borderColor: mrgbaToRgbaString(colors[1])
+          }}
+        >
           <div>
-            <PlayerName>{players[1]}</PlayerName>
+            <div>
+              <PlayerName>{players[1]}</PlayerName>
+            </div>
+            <div className={styles.race}>
+              {raceMap[racesShort[1]]}
+            </div>
           </div>
-          <div className={styles.race}>
-            {raceMap[racesShort[1]]}
+          <div>
+            {matchMeta.results[1] === 'Win' ? <WinnerIcon /> : <LoserIcon />}
           </div>
         </div>
       </div>
