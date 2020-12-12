1cffbd054214627a5310c7af3cc8ad0c22f6c826
Thomas Schäfer <git@tschaefer.eu>
2020-12-12 18:34:23 +0100

    Add header to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card, Container } from 'semantic-ui-react'
+import { Card, Container, Header } from 'semantic-ui-react'
 import Link from 'next/link'
 import tournaments from '../data/tournaments/tournaments.json'
 import { TournamentCard } from '../src/Components/Common/TournamentCard/TournamentCard'
@@ -8,6 +8,7 @@ export default function Home () {
   return (
     <Container style={{ minHeight: 'calc(100vh - 171px) '}}>
       <div style={{ marginTop: 60, marginBottom: 60 }}>
+        <Header size={'huge'}>All Tournaments</Header>
         <Card.Group itemsPerRow={3} stackable={true}>
           {tournaments.map(tournament => (
             <Link href={`/tournament/${tournament.id}`} passHref={true}>
