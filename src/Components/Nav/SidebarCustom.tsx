e918b1b88dd36a5bf04c735043a21cfef8483a30
Thomas Schäfer <git@tschaefer.eu>
2020-12-12 17:35:35 +0100

    Add active states to sidebar items

--- src/Components/Nav/SidebarCustom.tsx
+++ src/Components/Nav/SidebarCustom.tsx
@@ -1,6 +1,7 @@
 import { Icon, Menu, Segment, Sidebar } from 'semantic-ui-react'
 import React from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 interface Props {
   tournamentId: string
@@ -8,6 +9,17 @@ interface Props {
 
 export const SidebarCustom: React.FC<Props> = (props) => {
   const { tournamentId, children } = props
+  const router = useRouter()
+  const path = router.asPath
+
+  const hrefs = {
+    overview: `/tournament/${tournamentId}`,
+    maps: `/tournament/${tournamentId}/maps`,
+    units: `/tournament/${tournamentId}/units`,
+    upgrades: `/tournament/${tournamentId}/upgrades`,
+    games: `/tournament/${tournamentId}/games`
+  }
+
   return (
     <Sidebar.Pushable
       as={Segment}
@@ -25,32 +37,32 @@ export const SidebarCustom: React.FC<Props> = (props) => {
         width='thin'
         style={{ height: '100%' }}
       >
-        <Link href={`/tournament/${tournamentId}`} passHref={true}>
-          <Menu.Item as={'a'}>
+        <Link href={hrefs.overview} passHref={true}>
+          <Menu.Item as={'a'} active={path === hrefs.overview}>
             <Icon name='eye' />
             Overview
           </Menu.Item>
         </Link>
-        <Link href={`/tournament/${tournamentId}/maps`} passHref={true}>
-          <Menu.Item as={'a'}>
+        <Link href={hrefs.maps} passHref={true}>
+          <Menu.Item as={'a'} active={path === hrefs.maps}>
             <Icon name='map outline' />
             Maps
           </Menu.Item>
         </Link>
-        <Link href={`/tournament/${tournamentId}/units`} passHref={true}>
-          <Menu.Item as={'a'}>
+        <Link href={hrefs.units} passHref={true}>
+          <Menu.Item as={'a'} active={path === hrefs.units}>
             <Icon name='group' />
             Units & Buildings
           </Menu.Item>
         </Link>
-        <Link href={`/tournament/${tournamentId}/upgrades`} passHref={true}>
-          <Menu.Item as={'a'}>
+        <Link href={hrefs.upgrades} passHref={true}>
+          <Menu.Item as={'a'} active={path === hrefs.upgrades}>
             <Icon name='angle double up' />
             Upgrades
           </Menu.Item>
         </Link>
-        <Link href={`/tournament/${tournamentId}/games`} passHref={true}>
-          <Menu.Item as={'a'}>
+        <Link href={hrefs.games} passHref={true}>
+          <Menu.Item as={'a'} active={path.startsWith(hrefs.games)}>
             <Icon name='sitemap' />
             All Games
           </Menu.Item>
