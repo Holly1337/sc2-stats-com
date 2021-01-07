c67e4125342a38915592bee7d070007279187745
Thomas Schäfer <git@tschaefer.eu>
2021-01-07 17:12:42 +0100

    Add debounce to search and guarantee that results are new

--- src/Components/Nav/TopBar/SearchBar.tsx
+++ src/Components/Nav/TopBar/SearchBar.tsx
@@ -1,11 +1,14 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Search, SearchResultData, SearchResultProps } from 'semantic-ui-react'
 import { useRouter } from 'next/router'
 import { splitName } from '../../../util/playerNames'
 import { getMapImageByName } from '../../Sections/Maps/mapsImages'
+import { useDebounce } from 'use-debounce'
 
 export const SearchBar = (props) => {
   const [search, setSearch] = useState('')
+  const timestampOfSearchRequest = useRef<number>(Date.now ())
+  const [debouncedSearch] = useDebounce(search, 400)
   const [loading, setLoading] = useState(false)
   const [results, setResults] = useState([])
   const router = useRouter()
@@ -13,17 +16,30 @@ export const SearchBar = (props) => {
   const handleSearchChange = async (event: React.MouseEvent, data: SearchResultData) => {
     let newValue = data.value
     setSearch(newValue)
-    if (newValue === '') {
+  }
+
+  const startSearch = async (searchString) => {
+    if (searchString === '') {
       setResults([])
       return
     }
+
+    const timestampBeforeRequest = Date.now()
     setLoading(true)
-    const response = await fetch(`/api/search?q=${newValue}`)
+    const response = await fetch(`/api/search?q=${searchString}`)
     const json = await response.json()
     const results = json.results
-    setResults(results)
-    setLoading(false)
+
+    if (timestampBeforeRequest > timestampOfSearchRequest.current) {
+      timestampOfSearchRequest.current = timestampBeforeRequest
+      setResults(results)
+      setLoading(false)
+    }
   }
+
+  useEffect(() => {
+    startSearch(debouncedSearch)
+  }, [debouncedSearch])
 
   const onResultSelect = (e: React.MouseEvent<HTMLDivElement>, data: SearchResultData) => {
     const item = data.result.item
