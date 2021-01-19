e276cec37df6530679ef5a6ba655ff145800a62c
Holly1337 <thomas.schaefer1337@gmx.de>
2021-01-19 17:22:37 +0100

    hotfix type error

--- src/util/paths.ts
+++ src/util/paths.ts
@@ -1,7 +1,7 @@
 import { loadTournaments } from './loadFile'
 
 export const getTournamentPaths = async () => {
-  const tournaments = await loadTournaments<Array<TournamentData>>()
+  const tournaments = await loadTournaments()
   return tournaments.map(tournament => ({
     params: { id: tournament.id }
   }))
